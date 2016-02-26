313499bbdd990591a84cb32d859fa7795273b047
黄轩 <huangxuan@fangdd.com>
2016-02-26 10:16:33 +0800

    Initial Commit

---
+++ HXTakePicture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HXTakePicture
+//
+//  Created by 黄轩 on 16/2/26.
+//  Copyright © 2016年 黄轩 blog.libuqing.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HXTakePicture/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HXTakePicture
+//
+//  Created by 黄轩 on 16/2/26.
+//  Copyright © 2016年 黄轩 blog.libuqing.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
